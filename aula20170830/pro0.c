5bf412993cabdf493ac32a0afe03a8ff53f6cab8
romulo2303 <jr.97@hotmail.com>
2017-08-30 10:45:31 -0300

    fim de aula 3008

---
+++ aula20170830/pro0.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int conv(char * str){
+	int i = 0;
+	int x = 0;
+	while(str[i]) {
+		x = x*10 + str[i] - 48; i++;
+	}
+	return x;
+}
+int main(int argc, char ** argv) {
+	if(argc >= 3)
+	printf ("Soma = %d\n", conv(argv[1]) + conv(argv[2]));
+	else
+	printf ("Faltam argumentos!\n");
+	return 0;
+}
