e252b605d2fa6a80aed486bb6ba7363aebbfcf30
romulo2303 <jr.97@hotmail.com>
2017-09-27 16:08:54 -0300

    fim de aula

--- aula20170927/lexico.c
+++ aula20170927/lexico.c
@@ -2,20 +2,29 @@
 #include<stdlib.h>
 #include<ctype.h>
 #include<string.h>
-#define N 256
+#define N 256 // Tamanho maximo das STRINGS
+#define LIM 5 //Quantidade de palavras para organizar
 int main(){
-    int i, j;
-    char str[5][N], ordem[5][N];
-    for(i = 0; i < 5 ; i++){
+    int i, j, pos = 0;
+    char str[LIM][N], ordem[LIM][N];
+    for(i = 0; i < LIM ; i++){
         printf("Informe a palavra %d: ", i+1);
         fgets(str[i],N,stdin); str[i][strlen(str[i])-1] = '\0';
     }
-    for(i = 0; i < 4; i++){
-            for( j = i + 1 ; j < 5; j++)
-        if(strcmp(str[i],str[j]) < 0)
-        ordem[j] = str[i];
+    for (i = 0; i < LIM; i++) //Normalizando as palavras
+        for(j = 0 ; j < N; j++)
+        str[i][j] = tolower(str[i][j]);
+    for (i = 0; i < LIM; i++) //Frescura para colocar a primeira maiscula
+        str[i][0] = toupper(str[i][0]);
+    for(i = 0; i < LIM; i++){
+        pos = 0;
+        for(j = 0 ; j < LIM; j++)
+            if(strcmp(str[i],str[j]) == 1)
+                pos++;
+        strcpy(ordem[pos],str[i]);
     }
-    for(i = 0; i < 5; i++)
-    printf("%s \n", ordem[i]);
+    for(i = 0; i < LIM; i++)
+    printf("\n------------------\n%s", ordem[i]);
+    printf("\n------------------\n");
     return EXIT_SUCCESS;
 }
